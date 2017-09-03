015c41363885a4a9fe03de432fc45425881178a3
alejandro <alejandro@alejandros-Air.hitronhub.home>
2017-09-03 00:06:33 -0700

    Update method implementation

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -10,8 +10,9 @@ import 'rxjs/add/operator/toPromise';
 export class HeroService {
     
     private heroesUrl = 'api/heroes';  // URL to web api
+    private headers = new Headers({'Content-Type': 'application/json'});
     
-constructor(private http: Http) { }
+    constructor(private http: Http) { }
  
     getHeroes(): Promise<Hero[]> {
       return this.http.get(this.heroesUrl)
@@ -31,5 +32,14 @@ constructor(private http: Http) { }
             .toPromise()
             .then(response => response.json().data as Hero)
             .catch(this.handleError);
+    }
+    
+    update(hero: Hero): Promise<Hero> {
+      const url = `${this.heroesUrl}/${hero.id}`;
+      return this.http
+        .put(url, JSON.stringify(hero), {headers: this.headers})
+        .toPromise()
+        .then(() => hero)
+        .catch(this.handleError);
     }
 }
